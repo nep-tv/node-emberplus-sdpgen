774ded8ea475a2947ff054ea9712eb6b0af3c69f
Henrik Halvorsen <henrik.halvorsen@lawo.com>
2019-02-17 17:08:02 +1100

    Initial merger functionality implemented

--- sdpgen.js
+++ sdpgen.js
@@ -27,7 +27,10 @@ const TreeServer = require(".").TreeServer;
 var jsonConfig = fs.readFileSync('config.json');
 var jsonTree = JSON.parse(jsonConfig);
 var sdpokerTree = JSON.parse(fs.readFileSync('sdpokerTree.json'));
+var sdpmergerTree = JSON.parse(fs.readFileSync('sdpmergerTree.json'));
+
 jsonTree[0].children.push(sdpokerTree);
+jsonTree[0].children.push(sdpmergerTree);
 
 var objEmberTree = TreeServer.JSONtoTree(jsonTree);
 
@@ -56,6 +59,32 @@ server.on("value-change", (element) => {
         let output = element._parent.getElementByIdentifier("SDPoker-output");
         let poke = sdpoker.checkST2110(element.contents.value, {});
         output.update({ contents: { value: poke.map(e => e ? e.message + "\n" : undefined)} });
+    } else if (["SDP-1", "SDP-2", "SDP-3", "SDP-4"].includes(element.contents.identifier)) {
+        let sdp1 = element._parent.getElementByIdentifier("SDP-1").contents.value;
+        let sdp2 = element._parent.getElementByIdentifier("SDP-2").contents.value;
+        let sdp3 = element._parent.getElementByIdentifier("SDP-3").contents.value;
+        let sdp4 = element._parent.getElementByIdentifier("SDP-4").contents.value;
+        let groupedSdp = element._parent.getElementByIdentifier("Merged-SDP");
+        let output = element._parent.contents.sdp.join("\n") + "\n";
+
+        if (sdp1 !== '' && sdp2 !== '' && sdp3 !== '' && sdp4 !== '') {
+            
+            output += stripSdpHeader(sdp1) + stripSdpHeader(sdp2) + stripSdpHeader(sdp3) + stripSdpHeader(sdp4);
+
+            groupedSdp.update({ contents: { value: output } });
+        }
+
     }
 });
-    
+
+function stripSdpHeader(sdp) {
+    //console.log(sdp);
+    let lines = sdp.split("\n");
+    let outputArray = [];
+    let headerDone = false;
+    for (let l of lines) {
+        if (l.startsWith("m=") && !headerDone) { headerDone = true; }
+        if (headerDone && l !== '') { outputArray.push(l); }
+    }
+    return outputArray.join("\n");
+}
